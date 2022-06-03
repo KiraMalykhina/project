9c2131420ad668fd88990f3a242d4435846becaf
Malykhina Kira <example@gmail.com>
2022-06-03 12:43:28 +0300

    add h3

--- script.js
+++ script.js
@@ -19,4 +19,18 @@
 // console.log(typeof(answers));
 // console.log(typeof(null));
 
-``
+console.log('arr' + " - object");
+
+let incr = 10,
+    decr = 10;
+
+    // ++incr;
+    // --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+const isChecked = false,
+      isClose = false;
+
+console.log(isChecked || !isClose);
